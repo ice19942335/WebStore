eae5cf8e75e74a9bcecfa7d97a965e5013c7d6ab
ice19942335 <ice19942335@gmail.com>
2019-01-31 16:01:06 +0000

    Добавлены методы управления сотрудниками(Edit,Delete,Add)

--- WebStore/Controllers/EmployeeController.cs
+++ WebStore/Controllers/EmployeeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
@@ -35,12 +36,62 @@ namespace WebStore.Controllers
             return View(employee);
         }
 
-        public IActionResult Commit(int id, string firstName = "null", string sureName = "null", string patronymic = "null", int age = -1)
+        public IActionResult Commit()
         {
-            if (_employeesData.Commit(id, firstName, sureName, patronymic, age))
-                return View("Details", _employeesData.GetById(id));
-            else
-                return NotFound();
+            return NotFound();
         }
+
+        [Route("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            EmployeeView model;
+            if (id.HasValue)
+            {
+                model = _employeesData.GetById(id.Value);
+                if (ReferenceEquals(model, null))
+                    return NotFound();
+            }
+            else
+            {
+                model = new EmployeeView();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(EmployeeView model)
+        {
+            if (model.Id > 0)
+            {
+                var dbItem = _employeesData.GetById(model.Id);
+
+                if (ReferenceEquals(dbItem, null))
+                    return NotFound();// возвращаем результат 404 Not Found
+
+                dbItem.FirstName = model.FirstName;
+                dbItem.SurName = model.SurName;
+                dbItem.Age = model.Age;
+                dbItem.Patronymic = model.Patronymic;
+                dbItem.Position = model.Position;
+            }
+            else
+            {
+                _employeesData.AddNew(model);
+            }
+            //_employeesData.Commit();
+
+            return RedirectToAction(nameof(EmployeeList));
+        }
+
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            _employeesData.Delete(id);
+            return RedirectToAction(nameof(EmployeeList));
+        }
+
     }
+
 }
