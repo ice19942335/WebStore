b7a6a7aa174b3d844412d14f709c1c52d1eca3b2
ice19942335 <ice19942335@gmail.com>
2019-02-04 10:19:26 +0000

    Added Left side Bar

---
+++ WebStore/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Models
+{
+    /// <inheritdoc cref="NamedEntity" />
+    /// <summary>
+    /// Сущность продукт
+    /// </summary>
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        /// <summary>
+        /// Секция, к которой принадлежит товар
+        /// </summary>
+        public int SectionId { get; set; }
+
+        /// <summary>
+        /// Бренд товара
+        /// </summary>
+        public int? BrandId { get; set; }
+
+        /// <summary>
+        /// Ссылка на картинку
+        /// </summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+
+}
---
+++ WebStore/Models/ProductFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    /// <summary>
+    /// Класс для фильтрации товаров
+    /// </summary>
+    public class ProductFilter
+    {
+        /// <summary>
+        /// Секция, к которой принадлежит товар
+        /// </summary>
+        public int? SectionId { get; set; }
+
+        /// <summary>
+        /// Бренд товара
+        /// </summary>
+        public int? BrandId { get; set; }
+    }
+
+}
