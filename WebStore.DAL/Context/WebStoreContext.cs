99ae8fcbeb3033e7acd0bc6c2a9616d6562a87eb
ice19942335 <ice19942335@gmail.com>
2019-02-12 15:16:00 +0000

    Added entities Order, OrderItems. Migration + DB updated

--- WebStore.DAL/Context/WebStoreContext.cs
+++ WebStore.DAL/Context/WebStoreContext.cs
@@ -15,5 +15,9 @@ namespace WebStore.DAL.Context
         public DbSet<Section> Sections { get; set; }
 
         public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
---
+++ WebStore.Entities/Entities/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Entities.Entities.Base;
+
+namespace WebStore.Entities.Entities
+{
+    public class OrderItem : BaseEntity
+    {
+        public virtual OrderItem Order { get; set; }
+        
+        public virtual  Product Product { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
