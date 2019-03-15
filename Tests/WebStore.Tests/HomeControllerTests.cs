9229f48de59769c50fdc532ba61f4ae0888d9859
ice19942335 <ice19942335@gmail.com>
2019-03-15 18:01:58 +0000

    Added unit test project

---
+++ Tests/WebStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Controllers;
+using WebStore.Interfaces.services;
+
+namespace WebStore.Tests
+{
+    public class HomeControllerTests
+    {
+        
+    }
+}
